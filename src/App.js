e05401cfca305382578c54158fd8d8388234a7e1
Adam-Young <awy6448@gmail.com>
2020-10-12 23:41:34 -0400

    Boxes in the grid are now select-able.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import Grid from "./components/Grid"
 
 function App() {
   const [generations, setGenerations] = useState(0)
-  
   const speed = 100
   const rows = 30
   const cols = 50
-const [gridFull, setGridFull] = useState(Array(rows).fill().map(() => cols).fill(false))
+  const [gridFull, setGridFull] = useState(Array(rows).fill().map(() => Array(cols).fill(false)))
+
+  const selectBox = (row, col) => {
+    let gridCopy = arrayClone(gridFull)
+    gridCopy[row][col] = !gridCopy[row][col]
+    setGridFull(gridCopy)
+  }
+
+  
 
   return (
     <div className="App">
@@ -17,10 +24,15 @@ const [gridFull, setGridFull] = useState(Array(rows).fill().map(() => cols).fill
         cols={cols}
         gridFull={gridFull}
         rows={rows}
+        selectBox={selectBox}
         />
       <h2>Generations: {generations}</h2>
     </div>
   );
+}
+
+function arrayClone(arr) {
+  return JSON.parse(JSON.stringify(arr))
 }
 
 export default App;
