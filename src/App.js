0667ece792de4ea417f5636415c9ae9e6afea1f0
Adam-Young <awy6448@gmail.com>
2020-10-14 23:34:15 -0400

    MVP met

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import './App.css';
 import Grid from "./components/Grid"
 
 function App() {
-  const [isON, setIsOn] = useState(false)
-  const speed = 100
   const rows = 30
   const cols = 50
-  let intervalId = 0
   const [gridFull, setGridFull] = useState(Array(rows).fill().map(() => Array(cols).fill(false)))
   const [generations, setGenerations] = useState(0)
 
+  const [running, setRunning] = useState(false)
+  const [speed, setSpeed] = useState(100)
+  const [formData, setFormData] = useState({speed: 100})
+
   const selectBox = (row, col) => {
+    if (running){
+      return
+    }
     let gridCopy = arrayClone(gridFull)
     gridCopy[row][col] = !gridCopy[row][col]
     setGridFull(gridCopy)
   }
 
   const playButton = () => {
-    if (intervalId) clearInterval(intervalId)
-    intervalId = setInterval(play, speed)
+    setRunning(!running)
   }
 
-  const stopButton = e => {
-    clearInterval(intervalId)
+  const clearButton = () => {
+    setGridFull(Array(rows).fill().map(() => Array(cols).fill(false)))
+    setGenerations(0)
   }
 
-  const play = () => {
+
+  const randomButton = () => {
+    seed()
+  }
+
+  const seed = () => {
+    let gridCopy = arrayClone(gridFull)
+    for (let i = 0; i < rows; i++){
+      for (let j = 0; j < cols; j++){
+        if (Math.floor(Math.random() * 4) === 1) {
+          gridCopy[i][j] = true
+        }
+      }
+    }
+    setGridFull(gridCopy)
+  }
+  const step = () => {
     let g = gridFull
-    let g2 = arrayClone(gridFull)
+    let g2 = arrayClone(gridFull) // my buffer
+    
     for (var i = 0; i < rows; i++){
       for (var j = 0; j < cols; j++){
         let count = 0 // will count how meny live tiles for given gridbox
@@ -42,18 +63,72 @@ function App() {
         if (i < rows - 1 && j < cols - 1) if (g[i + 1][j + 1]) count++ //bottom right
         if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false
         if (!g[i][j] && count === 3) g2[i][j] = true
+    
       }
     }
+    
+    setGenerations(g => g + 1)
     setGridFull(g2)
-    setGenerations(generations => generations + 1)
-    }
+    return
+  }
   
+  
+  const play = async () => {
+    if (!running){
+      return
+    }
+    let g = gridFull
+    let g2 = arrayClone(gridFull) // my buffer
+    
+    for (var i = 0; i < rows; i++){
+      for (var j = 0; j < cols; j++){
+        let count = 0 // will count how meny live tiles for given gridbox
+        if (i > 0) if (g[i - 1][j]) count++ // top
+        if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++ //top left
+        if (i > 0 && j < cols - 1) if (g[i - 1][j + 1]) count++ // top right
+        if (j < cols - 1) if (g[i][j + 1]) count++ //right
+        if (j > 0) if (g[i][j - 1]) count++ //left
+        if (i < rows - 1 ) if (g[i + 1][j]) count++ //bottom
+        if (i < rows - 1 && j > 0) if (g[i + 1][j - 1]) count++ //bottom left
+        if (i < rows - 1 && j < cols - 1) if (g[i + 1][j + 1]) count++ //bottom right
+        if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false
+        if (!g[i][j] && count === 3) g2[i][j] = true
+    
+      }
+    }
+    
+    setGenerations(g => g + 1)
+    setGridFull(g2)
+    return
+  }
+    
+  useEffect(() => {
+    if (running) {
+      setTimeout(() => play(), speed)
+    }else{
+      return
+    }
+  }, [gridFull, running])
+
+  const handleChange = e => {
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
 
   return (
     <div className="App">
-      <h1>The Game Of Life</h1>
-      <button onClick={() => playButton()}>Start</button>
-      <button onClick={() => stopButton()}>stop</button>
+      <h1>Conways Game Of Life</h1>
+      <div className="center">
+        <button onClick={playButton}>
+        {running ? "stop" : "start"}
+        </button>
+      
+      <button disabled={running} onClick={clearButton}>clear</button>
+      <button disabled={running} onClick={randomButton}>Random</button>
+      <button disabled={running} onClick={() => step()}>step</button>
+      <button onClick={() => setSpeed(formData.speed)}>Set Speed(milsec)</button>
+      <input name="speed" value={formData.speed} onChange={e => handleChange(e)}/>
+      </div>
+      
       <Grid
         cols={cols}
         gridFull={gridFull}
@@ -61,6 +136,13 @@ function App() {
         selectBox={selectBox}
         />
       <h2>Generations: {generations}</h2>
+      <div className="center">
+      <h3>Rules</h3>
+      <p>1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.</p>
+      <p>2. Any live cell with two or three live neighbours lives on to the next generation.</p>
+      <p>3. Any live cell with more than three live neighbours dies, as if by overpopulation.</p>
+      <p>4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</p>
+      </div>
     </div>
   );
 }
