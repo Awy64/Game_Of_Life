cb9061356e1f47c16181206b91aac5bcc271487f
Adam-Young <awy6448@gmail.com>
2020-10-14 23:52:22 -0400

    Updates

---
+++ precache-manifest.0905b8779eb1962e0427bdb8fcfaf148.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab55722a6784dc623369ca62b7ff78b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db30fcea63f3bc2566bb",
+    "url": "/static/css/main.ee1541a3.chunk.css"
+  },
+  {
+    "revision": "4efb3d6fe6c8b0b0a0ec",
+    "url": "/static/js/2.f1889d26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f1889d26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db30fcea63f3bc2566bb",
+    "url": "/static/js/main.a2bc0564.chunk.js"
+  },
+  {
+    "revision": "cb6bf74422421c02dc4f",
+    "url": "/static/js/runtime-main.02afef44.js"
+  }
+]);
